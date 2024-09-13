e67c1517c06c055df03746ed651bc92c41dc712e
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-09-13 23:07:06 +0530

    Gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,52 @@
+import img1 from "../images/house.jpg";
+import img2 from "../images/logo.png";
+import img3 from "../images/sunset.jpg";
+
+import { useState } from "react";
+
 const Gallery = () => {
-  return <h2>Gallery</h2>;
+  const [images, setImages] = useState([
+    img1,img2,img3
+  ]);
+
+  const handleImageChange = (e) => {
+    const fileList = Array.from(e.target.files);
+    const imageUrls = fileList.map((file) => URL.createObjectURL(file));
+    setImages((prevImages) => [...prevImages, ...imageUrls]);
+  };
+
+  const handleButtonClick = () => {
+    document.getElementById("imageInput").click();
+  };
+  return (
+    <div className="relative bg-gray-600 rounded-xl h-1/3 pt-6">
+      <div className="flex justify-between items-center mb-4 pl-8">
+        <h2 className="bg-black text-white m-2 p-2 rounded-xl">Gallery</h2>
+        <button
+          className="m-2 p-2 bg-gray-500 rounded-xl"
+          onClick={handleButtonClick}
+        >
+          {" "}
+          + Add image
+        </button>
+        <input
+          type="file"
+          id="imageInput"
+          multiple
+          accept="image/*"
+          className="hidden"
+          onChange={handleImageChange}
+        />
+      </div>
+
+      <div className="flex space-x-4 pl-8">
+        {images.map((image, index) => (
+          <div key={index} className="w-24 h-24 bg-gray-600">
+            <img src={image} alt="imageeeee" className="w-full h-full object-cover rounded-xl"/>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Gallery;
